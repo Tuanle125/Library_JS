7c91d33ea1df831a0b9c737b1c34507b1b3b2e80
Tuanle125 <lda.tuan1912@gmail.com>
2023-12-05 00:30:24 +0700

    Add Book function

--- script.js
+++ script.js
@@ -1,8 +1,30 @@
+const Empty_Img_Path = "./img/books/empty.svg";
 const library = [];
+document.addEventListener("DOMContentLoaded", () => {
+    const btn_addBookDialog = document.querySelector("#add-book-dialog");
+    const formBook = document.querySelector(".dialog-book");
+    btn_addBookDialog.addEventListener("click", () => {
+        formBook.showModal();
+    });
 
-function Book(title, desc, imgPath) {
+    const btn_addBook = document.querySelector("#add-book");
+    const title = document.querySelector("#title");
+    const author = document.querySelector("#author");
+    const imgLink = document.querySelector("#img-link");
+    btn_addBook.addEventListener("click", (e) => {
+        const book = new Book(title.value, author.value, imgLink.value ? imgLink.value : Empty_Img_Path);
+        library.push(book);
+        createBookCard(book);
+        formBook.close();
+        // e.preventDefault();
+    });
+});
+
+
+
+function Book(title, author, imgPath) {
     this.title = title;
-    this.desc = desc;
+    this.author = author;
     this.imgPath = imgPath;
 }
 
@@ -10,25 +32,22 @@ function addBookToLib(book) {
     library.push(book);
 }
 
-function createBookStyle(book){
+function createBookCard(book){
     const cardList = document.querySelector(".card-list");
     const card = document.createElement("div");
     card.classList.add("card2");
 
     const title = document.createElement("div");
-    const desc = document.createElement("div");
+    const author = document.createElement("div");
     const img = document.createElement("img");
-    title.classList.add("txt2", "truncate1");
-    desc.classList.add("description", "truncate2");
+    title.classList.add("title", "truncate1");
+    author.classList.add("author", "truncate2");
     img.classList.add("img-book");
 
     img.src = book.imgPath;
     title.innerHTML = book.title;
-    desc.innerHTML = book.desc;
+    author.innerHTML = book.author;
 
-    card.append(img, title, desc);
+    card.append(img, title, author);
     cardList.append(card);
 }
-
-let dungViec = new Book("Dung Viec", "afalksfjkds ksdfjsdkf fksdfj", "https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1630654893i/27840432.jpg");
-createBookStyle(dungViec);
