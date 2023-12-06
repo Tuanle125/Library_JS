50b8d21318a88aecd824b0b92f6371d1f3c7a3f8
Tuanle125 <lda.tuan1912@gmail.com>
2023-12-06 11:52:22 +0700

    checkbox IsRead create check icon below card

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const Empty_Img_Path = "./img/books/empty.svg";
+const Check_Img_Path = "./img/icon/check.svg";
 const library = [];
 document.addEventListener("DOMContentLoaded", () => {
     const btn_addBookDialog = document.querySelector("#add-book-dialog");
@@ -11,8 +12,13 @@ document.addEventListener("DOMContentLoaded", () => {
     const title = document.querySelector("#title");
     const author = document.querySelector("#author");
     const imgLink = document.querySelector("#img-link");
+    const isRead = document.querySelector("#is-read");
     btn_addBook.addEventListener("click", (e) => {
-        const book = new Book(title.value, author.value, imgLink.value ? imgLink.value : Empty_Img_Path);
+        const book = new Book(
+            title.value, 
+            author.value, 
+            imgLink.value ? imgLink.value : Empty_Img_Path,
+            isRead.checked); console.log(isRead.checked);
         library.push(book);
         createBookCard(book);
         formBook.close();
@@ -22,10 +28,11 @@ document.addEventListener("DOMContentLoaded", () => {
 
 
 
-function Book(title, author, imgPath) {
+function Book(title, author, imgPath, isRead) {
     this.title = title;
     this.author = author;
     this.imgPath = imgPath;
+    this.isRead = isRead;
 }
 
 function addBookToLib(book) {
@@ -40,14 +47,25 @@ function createBookCard(book){
     const title = document.createElement("div");
     const author = document.createElement("div");
     const img = document.createElement("img");
+    const subContainer = document.createElement("div");
+
     title.classList.add("title", "truncate1");
     author.classList.add("author", "truncate2");
     img.classList.add("img-book");
+    subContainer.classList.add("author-readed");
 
     img.src = book.imgPath;
+    
     title.innerHTML = book.title;
     author.innerHTML = book.author;
 
-    card.append(img, title, author);
+    subContainer.append(author);
+    if(book.isRead){
+        const readed = document.createElement("img");
+        readed.classList.add("readed", "untouchable");
+        readed.src = Check_Img_Path;
+        subContainer.append(readed);
+    }
+    card.append(img, title, subContainer);
     cardList.append(card);
 }
